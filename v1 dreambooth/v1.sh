02818a33f524ee080bfe1bbbd9688b0bd14f27ec
yash <69687926+yashdevelopment@users.noreply.github.com>
2023-01-01 22:36:14 -0500

    Create v1.sh

---
+++ v1 dreambooth/v1.sh
@@ -0,0 +1,16 @@
+export MODEL_NAME="CompVis/stable-diffusion-v1-4"
+export INSTANCE_DIR="all_wsi/"
+export OUTPUT_DIR="model1/"
+
+accelerate launch diffusers/examples/dreambooth/train_dreambooth.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --instance_prompt="a photo of renal cell carcinoma" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=2e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --max_train_steps=650
